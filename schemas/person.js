8c2599677f9d2867a4812e67dbb578057871248f
QualityPre <qualitypre@protonmail.com>
2022-03-24 14:05:27 +0000

    added custom

---
+++ schemas/person.js
@@ -0,0 +1,40 @@
+import { MdPerson } from 'react-icons/md';
+
+export default {
+  // name for computer
+  name: 'person',
+  // name for user
+  title: 'Slicemasters',
+  type: 'document',
+  icon: MdPerson,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a little about the person...',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,74 +1,71 @@
-import {MdLocalPizza} from 'react-icons/md'
+import { MdLocalPizza } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
-    // name for computer
-    name:'pizza',
-    // name for user
-    title:'Pizzas',
-    type:'document',
-    icon:MdLocalPizza,
-    fields:[
+  // name for computer
+  name: 'pizza',
+  // name for user
+  title: 'Pizzas',
+  type: 'document',
+  icon: MdLocalPizza,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      inputComponent: PriceInput,
+      validation: (Rule) => Rule.min(1000).max(50000),
+    },
+    // add custom input component for toppings
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
         {
-            name:'name',
-            title:'Pizza Name',
-            type:'string',
-            description:'Name of the pizza'
+          type: 'reference',
+          to: [{ type: 'topping' }],
         },
-        {
-            name:'slug',
-            title:'Slug',
-            type:'slug',
-            options:{
-                source:'name',
-                maxLength:100
-            }
-        },
-        {
-            name:'image',
-            title:'Image',
-            type:'image',
-            options:{
-               hotspot:true
-            }
-        },
-        {
-            name:'price',
-            title:'Price',
-            type:'number',
-            description:'Price of the pizza in cents',
-            validation:Rule=>Rule.min(1000).max(50000)
-            
-        },
-        // add custom input component for toppings
-        {
-            name:'toppings',
-            title:'Toppings',
-            type:'array',
-            of:[
-                {
-                    type:'reference',
-                    to:[{
-                        type:'topping'
-                    }]
-                }
-            ]
-
-        }
-    ],
-    // custom preview
-    preview:{
-        select:{
-            title:'name',
-            media:'image',
-            topping0:'toppings.0.name',
-            topping1:'toppings.1.name',
-            topping2:'toppings.2.name',
-            topping3:'toppings.3.name'
-        },
-        prepare:({title,media,...toppings})=>{
-         const tops = Object.values(toppings).filter(Boolean)
-         return {title,media,subtitle:tops.join(',')}
-        }
-
-    }
-}
+      ],
+    },
+  ],
+  // custom preview
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      return { title, media, subtitle: tops.join(',') };
+    },
+  },
+};
